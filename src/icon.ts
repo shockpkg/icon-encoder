a36260e12839a90d3496d4d4fd17c5064354e7e6
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-10-07 03:56:36 -0400

    Expose useful utility functions

--- src/icon.ts
+++ src/icon.ts
@@ -1,7 +1,5 @@
-import {PNG} from 'pngjs';
-
 import {IImageData} from './types';
-import {concatUint8Arrays, pngRepack} from './util';
+import {encodePngToRgba, encodeRgbaToPng} from './util';
 
 /**
  * Icon object.
@@ -19,25 +17,7 @@ export abstract class Icon {
 	 * @returns Image data.
 	 */
 	protected async _decodePngToRgba(data: Readonly<Uint8Array>) {
-		const png = new PNG();
-		await new Promise((resolve, reject) => {
-			png.parse(
-				Buffer.from(data.buffer, data.byteOffset, data.byteLength),
-				err => {
-					if (err) {
-						reject(err);
-						return;
-					}
-					resolve(png);
-				}
-			);
-		});
-		const {width, height, data: d} = png;
-		return {
-			width,
-			height,
-			data: new Uint8Array(d.buffer, d.byteOffset, d.byteLength)
-		} as IImageData;
+		return encodePngToRgba(data);
 	}
 
 	/**
@@ -51,25 +31,7 @@ export abstract class Icon {
 		imageData: Readonly<IImageData>,
 		srgb: number | null = null
 	) {
-		const {width, height, data} = imageData;
-		const png = new PNG({
-			width,
-			height,
-			deflateLevel: 9,
-			deflateStrategy: 1,
-			deflateChunkSize: 32 * 1024
-		});
-		png.data = Buffer.from(data.buffer, data.byteOffset, data.byteLength);
-		const packed: Uint8Array[] = [];
-		await new Promise((resolve, reject) => {
-			png.on('data', (d: Buffer) => {
-				packed.push(d);
-			});
-			png.on('error', reject);
-			png.on('end', resolve);
-			png.pack();
-		});
-		return pngRepack(concatUint8Arrays(packed), srgb);
+		return encodeRgbaToPng(imageData, srgb);
 	}
 
 	/**
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
-import {IPngIhdr} from './types';
+import {PNG} from 'pngjs';
+
+import {IImageData, IPngIhdr} from './types';
 
 const PNG_MAGIC = [137, 80, 78, 71, 13, 10, 26, 10];
 const PNG_MAGIC_SIZE = 8;
@@ -54,6 +56,66 @@ export function concatUint8Arrays(arrays: Readonly<Readonly<Uint8Array>[]>) {
 		i += a.length;
 	}
 	return r;
+}
+
+/**
+ * Decode PNG data to RGBA image.
+ *
+ * @param data PNG data.
+ * @returns Image data.
+ */
+export async function encodePngToRgba(data: Readonly<Uint8Array>) {
+	const png = new PNG();
+	await new Promise((resolve, reject) => {
+		png.parse(
+			Buffer.from(data.buffer, data.byteOffset, data.byteLength),
+			err => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(png);
+			}
+		);
+	});
+	const {width, height, data: d} = png;
+	return {
+		width,
+		height,
+		data: new Uint8Array(d.buffer, d.byteOffset, d.byteLength)
+	} as IImageData;
+}
+
+/**
+ * Encode RGBA image to PNG data.
+ *
+ * @param imageData Image data.
+ * @param srgb SRGB mode.
+ * @returns PNG data.
+ */
+export async function encodeRgbaToPng(
+	imageData: Readonly<IImageData>,
+	srgb: number | null = null
+) {
+	const {width, height, data} = imageData;
+	const png = new PNG({
+		width,
+		height,
+		deflateLevel: 9,
+		deflateStrategy: 1,
+		deflateChunkSize: 32 * 1024
+	});
+	png.data = Buffer.from(data.buffer, data.byteOffset, data.byteLength);
+	const packed: Uint8Array[] = [];
+	await new Promise((resolve, reject) => {
+		png.on('data', (d: Buffer) => {
+			packed.push(d);
+		});
+		png.on('error', reject);
+		png.on('end', resolve);
+		png.pack();
+	});
+	return pngRepack(concatUint8Arrays(packed), srgb);
 }
 
 /**
